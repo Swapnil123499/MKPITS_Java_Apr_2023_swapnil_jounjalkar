738340afb2a39855d2a1d46712b67475aba85e71
Swapnil123499 <jounjalkarswapnil@gmail.com>
2023-06-20 18:05:10 +0530

    overloadding overriding

---
+++ Java/overridingexample1/src/main/java/org/example/Addition2.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Addition2 extends Addition {
+
+
+
+
+    @Override
+    public void add(int number1, int number2)
+    {
+
+        int sum=number1+number2;
+  System.out.println(sum);
+    }
+}
